b0e8802d87792e49c06d7d2984cbfb699eec92cb
Thiago Brasil <thiago@lepidus.com.br>
2025-11-28 15:22:09 -0400

    Update new plugin settings based on old plugin

--- ObsoleteVersionMigration.inc.php
+++ ObsoleteVersionMigration.inc.php
@@ -6,12 +6,38 @@
  */
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Capsule\Manager as Capsule;
 
 class ObsoleteVersionMigration extends Migration
 {
     public function up()
     {
         $this->deleteObsoletePlugin();
+
+        Capsule::table('plugin_settings')
+            ->where('plugin_name', 'doinosumarioplugin')
+            ->where('setting_name', 'enabled')
+            ->get(['context_id', 'setting_value'])
+            ->each(function ($row) {
+                # Enable or disable the new plugin based on the old plugin's settings
+                Capsule::table('plugin_settings')
+                    ->where('plugin_name', 'doiinsummaryplugin')
+                    ->where('context_id', $row->context_id)
+                    ->where('setting_name', 'enabled')
+                    ->update(['setting_value' => $row->setting_value]);
+
+                # Remove old plugin settings
+                Capsule::table('plugin_settings')
+                    ->where('plugin_name', 'doinosumarioplugin')
+                    ->where('context_id', $row->context_id)
+                    ->delete();
+            });
+
+        # Delete old plugin version record
+        Capsule::table('versions')
+            ->where('product_type', 'plugins.generic')
+            ->where('product', 'doiNoSumario')
+            ->delete();
     }
 
     private function deleteObsoletePlugin()
